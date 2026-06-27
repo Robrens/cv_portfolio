0a5327bbc8bfcc76ba5420778d5ccd5cc786c7a5
Jean-Baptiste Baudu <jean-baptiste.b@yeswedev.bzh>
2026-06-27 14:42:39 +0200

    feat(header): add header component with navigation links

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,18 @@
+<div class="header">
+  <div class="site-container">
+    <div class="w-full flex flex-row gap-10 align-middle">
+      <div class="w-[20%]">
+        <h1 class="title text-brand-accent inline">JB</h1>
+      </div>
+      <div class="flex flex-col justify-center w-[75%]">
+        <nav class="nav flex flex-row gap-10 w-full">
+          <a href="/" class="nav-link is-active">Accueil</a>
+          <a href="#parcours" class="nav-link">Parcours</a>
+          <a href="#competences" class="nav-link">Compétences</a>
+          <a href="#projets" class="nav-link">Projets</a>
+          <a href="#contact" class="nav-link">Contact</a>
+        </nav>
+      </div>
+    </div>
+  </div>
+</div>
