a70a1c77b668161918dc3899304552a3b2443160
Jean-Baptiste Baudu <jean-baptiste.b@yeswedev.bzh>
2026-07-02 17:13:52 +0200

    fix(about): adjust layout classes for improved grid structure and consistency

--- resources/views/components/layouts/about.blade.php
+++ resources/views/components/layouts/about.blade.php
@@ -10,7 +10,7 @@
       </h1>
     </div>
     <div class="mt-6 grid grid-cols-1 gap-8 md:grid-cols-5">
-      <div class="md:col-span-2">
+      <div class="col-span-1 md:col-span-2">
         <p class="mt-6 text-heading">
           Ma double compétence en développement applicatif et en système & réseau me permet d'intervenir sur l'ensemble
           deu
@@ -20,11 +20,11 @@
           Je m'attache à produire un code propre, des infrastructures fiables et des processus automatisés.
         </p>
       </div>
-      <div class="md:col-span-3 grid grid-cols-4 gap-4">
+      <div class="grid col-span-1 md:col-span-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         <x-ui.card icon="code-bracket" title="5+" subtitle="années" description="Développement applicatif" />
-        <x-ui.card icon="code-bracket" title="5+" subtitle="années" description="Développement applicatif" />
-        <x-ui.card icon="code-bracket" title="5+" subtitle="années" description="Développement applicatif" />
-        <x-ui.card icon="code-bracket" title="5+" subtitle="années" description="Développement applicatif" />
+        <x-ui.card icon="server-stack" title="3+" subtitle="années" description="Système & Réseau" />
+        <x-ui.card icon="rocket-launch" title="10+" subtitle="projets" description="Réalisation de solutions métiers" />
+        <x-ui.card icon="academic-cap" title="3" subtitle="diplômes" description="Titres homologués par l'État" />
       </div>
     </div>
   </div>
