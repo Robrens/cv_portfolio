3b779b532430b4b50c4c725fe6087e1e42059b2f
Jean-Baptiste Baudu <jean-baptiste.b@yeswedev.bzh>
2026-07-24 13:42:15 +0200

    feat(work-methods): add WorkMethod resource with CRUD pages and schema configuration

---
+++ app/Filament/Resources/WorkMethods/Schemas/WorkMethodForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\WorkMethods\Schemas;
+
+use Filament\Schemas\Schema;
+
+class WorkMethodForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
