163177a1072b0581ef400936d01927c09c88f11e
Jean-Baptiste Baudu <jean-baptiste.b@yeswedev.bzh>
2026-07-24 11:10:27 +0200

    feat(models): add fillable attributes and casts to WorkMethod model

--- app/Models/WorkMethod.php
+++ app/Models/WorkMethod.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class WorkMethod extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'icon',
+        'sort_order',
+        'is_active',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'sort_order' => 'integer',
+            'is_active' => 'boolean',
+        ];
+    }
 }
