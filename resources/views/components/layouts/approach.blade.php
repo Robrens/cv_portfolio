3da45e9bb6c2d95d4d2cc6c001f5edfb283c7761
Jean-Baptiste Baudu <jean-baptiste.b@yeswedev.bzh>
2026-07-24 15:02:03 +0200

    feat(approach): refactor approach section to use dynamic work methods data and improve layout

--- resources/views/components/layouts/approach.blade.php
+++ resources/views/components/layouts/approach.blade.php
@@ -1,3 +1,8 @@
+@props([
+'workMethods',
+])
+
+@if ($workMethods->isNotEmpty())
 <section class="section approach">
   <div class="site-container rounded-card bg-brand-primary">
     <div class="mb-5 md:mb-8 lg:mb-12">
@@ -9,65 +14,31 @@
         Ma façon de travailler
       </h1>
     </div>
-    <div>
-      <ul class="flex flex-row flex-wrap">
-        <li class="approach-item">
-          <div class="flex flex-col col-span-1">
-            <x-heroicon-o-code-bracket class="h-10 w-10 text-brand-accent" />
-          </div>
-          <div class="flex flex-col gap-1 md:gap-2 lg:gap-3 col-span-5">
-            <h3 class="text-lg font-bold text-white">
-              Développement
-            </h3>
-            <p class="text-sm leading-6 text-content-inverted">
-              Conception et développement d’applications web modernes,
-              API robustes et intégrations sur mesure.
-            </p>
-          </div>
-        </li>
-        <li class="approach-item">
-          <div class="flex flex-col col-span-1">
-            <x-heroicon-o-code-bracket class="h-10 w-10 text-brand-accent" />
-          </div>
-          <div class="flex flex-col gap-1 md:gap-2 lg:gap-3 col-span-5">
-            <h3 class="text-lg font-bold text-white">
-              Développement
-            </h3>
-            <p class="text-sm leading-6 text-content-inverted">
-              Conception et développement d’applications web modernes,
-              API robustes et intégrations sur mesure.
-            </p>
-          </div>
-        </li>
-        <li class="approach-item">
-          <div class="flex flex-col col-span-1">
-            <x-heroicon-o-code-bracket class="h-10 w-10 text-brand-accent" />
-          </div>
-          <div class="flex flex-col gap-1 md:gap-2 lg:gap-3 col-span-5">
-            <h3 class="text-lg font-bold text-white">
-              Développement
-            </h3>
-            <p class="text-sm leading-6 text-content-inverted">
-              Conception et développement d’applications web modernes,
-              API robustes et intégrations sur mesure.
-            </p>
-          </div>
-        </li>
-        <li class="approach-item">
-          <div class="flex flex-col col-span-1">
-            <x-heroicon-o-code-bracket class="h-10 w-10 text-brand-accent" />
-          </div>
-          <div class="flex flex-col gap-1 md:gap-2 lg:gap-3 col-span-5">
-            <h3 class="text-lg font-bold text-white">
-              Développement
-            </h3>
-            <p class="text-sm leading-6 text-content-inverted">
-              Conception et développement d’applications web modernes,
-              API robustes et intégrations sur mesure.
-            </p>
-          </div>
-        </li>
-      </ul>
-    </div>
+
+    <ul class="flex flex-row flex-wrap">
+      @foreach ($workMethods as $workMethod)
+      <li class="approach-item">
+        <div class="col-span-1 flex flex-col">
+          @if ($workMethod->icon)
+          <x-dynamic-component :component="'heroicon-o-' . $workMethod->icon" class="h-10 w-10 text-brand-accent" />
+          @endif
+        </div>
+
+        <div class="col-span-5 flex flex-col gap-1
+                    md:gap-2 lg:gap-3">
+          <h3 class="text-lg font-bold text-white">
+            {{ $workMethod->title }}
+          </h3>
+
+          @if ($workMethod->description)
+          <p class="text-sm leading-6 text-content-inverted">
+            {{ $workMethod->description }}
+          </p>
+          @endif
+        </div>
+      </li>
+      @endforeach
+    </ul>
   </div>
 </section>
+@endif
