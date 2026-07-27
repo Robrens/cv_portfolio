ea9e8eeee41112a6e3d5ddb3eaadf1bbde422669
Jean-Baptiste Baudu <jean-baptiste.b@yeswedev.bzh>
2026-07-27 14:52:40 +0200

    feat(database-seeder): prevent user seeding in non-local environments

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        if (! app()->isLocal()) {
+            return;
+        }
 
         User::factory()->create([
             'name' => 'Test User',
