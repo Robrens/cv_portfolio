b48554d01413e8d7aa6d59c91cfee3583960bd81
Jean-Baptiste Baudu <jean-baptiste.b@yeswedev.bzh>
2026-07-27 17:10:10 +0200

    feat(tests): enhance TestCase setup and teardown for compiled views management

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,33 @@
 
 namespace Tests;
 
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    private string $compiledViewsPath;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->compiledViewsPath = sys_get_temp_dir()
+            .DIRECTORY_SEPARATOR
+            .'cv-portfolio-tests-'
+            .getmypid()
+            .DIRECTORY_SEPARATOR
+            .'views';
+
+        (new Filesystem)->ensureDirectoryExists($this->compiledViewsPath);
+
+        config()->set('view.compiled', $this->compiledViewsPath);
+    }
+
+    protected function tearDown(): void
+    {
+        (new Filesystem)->deleteDirectory($this->compiledViewsPath);
+
+        parent::tearDown();
+    }
 }
