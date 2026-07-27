c05c4b7c73503fbda29977602853f63cc48f8b3e
Jean-Baptiste Baudu <jean-baptiste.b@yeswedev.bzh>
2026-07-27 13:55:22 +0200

    feat(routes): add routes for legal and privacy notices

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', HomeController::class)
     ->name('home');
+
+Route::view('/legal-notices', 'legal.legal-notices')
+    ->name('legal.mentions');
+
+Route::view('/privacy-notices', 'legal.privacy-notices')
+    ->name('legal.privacy');
