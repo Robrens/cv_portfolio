711185630cc70ff812baaf2a387a9ab1a9263039
Jean-Baptiste Baudu <jean-baptiste.b@yeswedev.bzh>
2026-07-28 14:07:53 +0200

    feat(seo): add routes for robots.txt and sitemap.xml handling

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SeoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)
@@ -11,3 +12,9 @@ Route::get('/legal-notices', HomeController::class)
 
 Route::get('/privacy-policy', HomeController::class)
     ->name('legal.privacy');
+
+Route::get('/robots.txt', [SeoController::class, 'robots'])
+    ->name('seo.robots');
+
+Route::get('/sitemap.xml', [SeoController::class, 'sitemap'])
+    ->name('seo.sitemap');
