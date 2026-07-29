487f969418aa1477272df51426a860c3f91e8005
Jean-Baptiste Baudu <jean-baptiste.b@yeswedev.bzh>
2026-07-29 11:44:16 +0200

    feat: add favicon and web manifest files for improved site branding

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,6 +12,14 @@
     <meta name="robots" content="index, follow" />
     
     <link rel="canonical" href="{{ route('home') }}" />
+
+    <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+    <meta name="theme-color" content="#040819">
     
     <meta property="og:title" content="{{ $seoTitle }}" />
     <meta property="og:description" content="{{ $seoDescription }}" />
